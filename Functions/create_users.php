8dfc1456b75dc71d1ec2bab7c0e8c18f825f1ae3
a5t0b <luo.james@alumni.ubc.ca>
2018-11-10 16:00:03 -0800

    Creater Users v1

--- Functions/create_users.php
+++ Functions/create_users.php
@@ -1,8 +1,9 @@
-<?php include "users.php"; ?>
+<?php include "applicants.php"; ?>
+<?php include "employers.php"; ?>
 <?php
 
 //Creat new normal user
-function createNormal() {
+function createApplicant() {
 	if(isset($_POST["submit"])) {
 		global $connection;
 		$username = mysqli_real_escape_string($connection, $_POST['username']);
@@ -28,3 +29,33 @@ function createNormal() {
 		}
 	}
 }
+
+//Creat new employer
+function createEmployer() {
+	if(isset($_POST["submit"])) {
+		global $connection;
+		$username = mysqli_real_escape_string($connection, $_POST['username']);
+		$password = mysqli_real_escape_string($connection, $_POST['password']);
+		$sin = mysqli_real_escape_string($connection, $_POST['sin']);
+		$contactinfo = mysqli_real_escape_string($connection, $_POST['contact_info']);
+		$name = mysqli_real_escape_string($connection, $_POST['name']);
+		$pysiologicalinfo = mysqli_real_escape_string($connection, $_POST['physiological_info']);
+		$workexperience = mysqli_real_escape_string($connection, $_POST['work_experience']);
+		$education = mysqli_real_escape_string($connection, $_POST['education']);
+		$position = mysqli_real_escape_string($connection, $_POST['position']);
+		$cname = mysqli_real_escape_string($connection, $_POST['company_name']);
+
+
+		$query = 'INSERT INTO users(username, password)';
+		$query .= "VALUES ('$username', '$password', '$sin','$contactinfo','$name','$pysiologicalinfo','$workexperience','$education', '$position', '$cname')";
+
+
+		$result = mysqli_query($connection, $query);
+		if (!$result) {
+			die("Query Failed" . mysqli_error($connection));
+		} else {
+			echo "Record Created";
+		}
+	}
+}
+
