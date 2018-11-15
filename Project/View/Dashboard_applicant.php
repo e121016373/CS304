564031bdb6c104e6ed8abb6a423eb21907d83aac
Scott Wang <wht972332930@163.com>
2018-11-14 22:41:59 -0800

    wht

--- Project/View/Dashboard_applicant.php
+++ Project/View/Dashboard_applicant.php
@@ -29,19 +29,50 @@ session_start();
 		if (!$result) {
 			die("Query Failed" . mysqli_error($connection));
 		}
-		echo "<form action =\"apply_job.php\" mthod =\"post\">";
+		echo "<form action =\"apply_job.php\" method =\"post\">";
 		echo "<table>"; // start a table tag in the HTML
 		echo "<tr><td>" . "JobID" . "</td><td>" . 'CompanyName' . "</td><td>" . 'Requirements' . "</td><td>" . 'Description' . "</td><td>" . 'Location' . "</td><td>" . 'Type' . "</td><td>" . 'Salary' . "</td></tr>"; 
 		while($row = mysqli_fetch_assoc($result)){   
 			echo "<tr><td>" . $row['JobID'] . "</td><td>" . $row['CompanyName'] . "</td><td>" . $row['Requirements'] . "</td><td>" . $row['Description'] . "</td><td>" . $row['Location'] . "</td><td>" . $row['Type'] . "</td><td>" . $row['Salary'] . "</td>";
-			echo "<td><button type = \"submit\" name = \"apply_job\" value = " . $row['JobID'] .">apply</button></td></tr>"; 
+			echo "<td><button type = \"submit\" name = \"apply_job\" value = ". $row['JobID'] . ">apply</button></td></tr>"; 
 		}
 		echo "</table>"; //Close the table in HTML
 		echo "</form>";
 	}
-	if (isset($_GET["view_my_reviews"])) {
-		$query = "SELECT * FROM postedjob";
+	if (isset($_GET["view_my_application"])) {
+		$query = "SELECT * FROM application NATURAL JOIN postedjob INNER JOIN employer ON employer.SIN = postedjob.Employer_SIN NATURAL JOIN person ";
+		$query .= "WHERE Applicant_SIN = " . $_SESSION['sin'];
 		$result = mysqli_query($connection, $query);
+		if (!$result) {
+			die("Query Failed" . mysqli_error($connection));
+		}
+		echo "<form action =\"cancel_application.php\" method =\"post\">";
+		echo "<table>"; // start a table tag in the HTML
+		echo "<tr><td>" . 'Job ID' . "</td><td>" . 'Company Name' . "</td><td>" . 'Employer Cantact_info' . "</td><td>" . 'Status' . "</td></tr>"; 
+		while($row = mysqli_fetch_assoc($result)){   
+			echo "<tr><td>" . $row['JobID'] . "</td><td>" . $row['CompanyName'] . "</td><td>" . $row['Contact_Info'] . "</td><td>" . "Null" . "</td>";
+			echo "<td><button type = \"submit\" name = \"cancel_job\" value = ". $row['ApplicationID'] . ">Cancel</button></td></tr>"; 
+		}
+		echo "</table>"; //Close the table in HTML
+		echo "</form>";
+	}
+	if (isset($_GET["view_my_schedule"])) {
+		$query = "SELECT * FROM evaluation NATURAL JOIN application NATURAL JOIN postedjob";
+		$result = mysqli_query($connection, $query);
+		if (!$result) {
+			die("Query Failed" . mysqli_error($connection));
+		}
+		echo "<table>"; // start a table tag in the HTML
+		echo "<tr><td>" . 'Job ID' . "</td><td>" . 'Company Name' . "</td><td>" . 'Date' . "</td><td>" . 'Time' . "</td><td>" . 'Length' . "</td><td>" . 'Type' . "</td><td>";
+		$row = mysqli_fetch_assoc($result);
+		//if (isset($row))
+		echo 'Type' . "</td><td>" . 'Date' . "</td></tr>"; 
+		while($row = mysqli_fetch_assoc($result)){   
+			echo "<tr><td>" . $row['ApplicationID'] . "</td><td>" . $row['JobID'] . "</td><td>" . $row['CompanyName'] . "</td><td>" . $row['Contact_Info'] . "</td><td>" . "Null" . "</td>";
+			echo "<td><button type = \"submit\" name = \"cancel_job\" value = ". $row['ApplicationID'] . ">Cancel</button></td></tr>"; 
+		}
+		echo "</table>"; //Close the table in HTML
+		echo "</form>";
 	}
 
 	?>
