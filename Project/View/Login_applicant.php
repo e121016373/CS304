bf83369757bbb4df2366e3bae33797d1d1426cdc
FAN FENG <e1j0b@ugrad.cs.ubc.ca>
2018-11-15 22:09:39 -0800

    format update

--- Project/View/Login_applicant.php
+++ Project/View/Login_applicant.php
@@ -13,7 +13,7 @@ include "../Controller/General.php";
 
 	
 ?>
-
+<link rel="stylesheet" type="text/css" href="template2.css"/>
 
 
 
@@ -23,6 +23,7 @@ include "../Controller/General.php";
 	<title>login</title>
 </head>
 <body>
+    <h1 style="background-color: transparent;margin-left:auto;margin-right:auto;display:block;margin-top:4%;margin-bottom:0%; border-radius: 12px; color: orange; font-size: 30px;text-align: center">Quick login to find suitable jobs!</h1>
 	<form action="Login_applicant.php" method="post">
 		<div class = "form-group">
 			<label for="username">Username</label>
@@ -34,8 +35,8 @@ include "../Controller/General.php";
 			<input type="password" name = "Password" class = "form-control">
 		</div>
 
-		<input type="submit" name="register" value = "register"></input> 
-		<input type="submit" name="submit" value="submit"></input>
+		<input type="submit" name="register" value = "register" style="background-color: tomato;margin-top: 2%;"></input> 
+		<input type="submit" name="submit" value="submit" style="background-color:tomato;margin-top: 2%;"></input>
 
 
 	</form>
